08eb2ebcff2dbb8eb5ec62ae67594b6c5c7cca49
taybson <taybson@gmail.com>
2015-07-22 18:16:04 -0400

    lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,86 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ // var work = {
+ // 	position:"Engineer",
+ // 	employer: "Google",
+ // 	years:5,
+ // }
+ // work.city="San Jose";
+
+ // var education={
+ // 	name:"University of Delaware",
+ // 	years:7,
+ // }
+ // education["city"]="Newark Delaware";
+
+ // $("#main").append(work.position);
+
+ // $("#main").append(education["city"]);
+
+
+ // var education={
+ // 	"schools":
+ // 	{
+ // 		"name":"University of Delaware",
+	//  	"cities":"Newark",
+	//  	"major":"CS"
+	//  	"graduations":"2017",
+	//  	"years":7,
+ // 	}	
+
+ // }
+
+
+var jobs = {
+	            "employer": "Google",
+	            "title": "Software Engineer",
+	            "location": "Mountaiin View",
+	            "dates": "2015",
+	            "description": "GoodJob"
+        	}
+
+var projects = {
+		            "title": "Online Test System",
+		            "dates": "2011",
+		            "description": "Very Useful",
+		            "images": "project.jpg"
+        		}
+
+var bio = {
+            "name": "Steven",
+            "role": "Software Engineer",
+            "welcomeMessage": "Hello World",
+            "contacts": [
+                {
+                    "mobile": "302-765-8515",
+                    "email": "stevenxzhou@gmail.com",
+                    "github": "github.com/stevenxzhou",
+                    "twitter": "twitter.com/stevenxzhou",
+                    "location": "San Jose"
+                }
+            ],
+            "skills": [
+                "java",
+                "mysql",
+                "python",
+                "javascript"
+            ]
+        }
+
+var education = {
+		            "name": "University of Delaware",
+		            "location": "Newark Delaware",
+		            "degree": "Master of Computer Sciences",
+		            "majors": "CS",
+		            "dates": "2015",
+		            "url": "www.udel.edu",
+		            "onlinecourses": [
+		                {
+		                    "title": "javascript",
+		                    "school": "udacity",
+		                    "url": "udacity.com"
+		                }
+		            ]
+        		}
